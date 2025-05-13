206241d51a795bbb6600849a9a178e1f9bfc00c6
driwCoder <driwcoder@gmail.com>
2025-05-13 10:33:05 -0300

    commit inicial, pagina de cards basica

---
+++ tailwind.config.ts
@@ -0,0 +1,38 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss';
+
+
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        fire: '#F08030',
+        water: '#6890F0',
+        grass: '#78C850',
+        electric: '#F8D030',
+        psychic: '#F85888',
+        ice: '#98D8D8',
+        dragon: '#7038F8',
+        dark: '#705848',
+        fairy: '#EE99AC',
+        normal: '#A8A878',
+        fighting: '#C03028',
+        flying: '#A890F0',
+        poison: '#A040A0',
+        ground: '#E0C068',
+        rock: '#B8A038',
+        bug: '#A8B820',
+        ghost: '#705898',
+        steel: '#B8B8D0',
+        primary: '#2563eb', // azul padrão usado no botão
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
